337301503469f8e8dd37c7da950fd548354e5a7b
lollor <loryraia2003@gmail.com>
2021-04-19 19:41:12 +0200

    Aggiunto compito tag select dinamico

---
+++ tag-select-dinamico/js/script.js
@@ -0,0 +1,9 @@
+function setup(){
+    document.getElementById("cognome").value = "";
+    document.getElementById("nome").value = "";
+    document.getElementById("data").value = "";
+    document.getElementById("sesso").selectedIndex = -1;
+    document.getElementById("coloreocchi").selectedIndex = -1;
+    document.getElementById("provincia").selectedIndex = -1;
+}
+setup()
